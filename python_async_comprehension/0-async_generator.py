3d78847372a9d0a652c2f4d3c3440f27eaf93720
myla2205 <myla2205@gmail.com>
2025-05-04 15:55:25 -0400

    com

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+0. Async Generator
+"""
+
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Asynchronous generator that yields a random number between 0 and 10.
+    It waits for 1 second before yielding each number.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+        
